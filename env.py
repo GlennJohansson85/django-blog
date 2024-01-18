dd93f5c202474d5e998c9ee8d6ced17c7ec28dbe
GlennJohansson85 <139962883+GlennJohansson85@users.noreply.github.com>
2024-01-18 23:19:49 +0100

    c

--- env.py
+++ env.py
@@ -1,13 +1,5 @@
 import os
 
-# Load environment variables from .env file
-from dotenv import load_dotenv
+os.environ.setdefault(
+    "DATABASE_URL", "postgres://ygkwkcqh:***@snuffleupagus.db.elephantsql.com/ygkwkcqh"
 
-load_dotenv()
-
-# Set default values for environment variables
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "codestar.settings")
-os.environ.setdefault("SECRET_KEY", "django-insecure-&m%gu-ez==#b@$+_t99xi_wgv()$4vr7#$-og^=x4rdhx6lh6j")
-os.environ.setdefault("DEBUG", "True")
-os.environ.setdefault("ALLOWED_HOSTS", "djang.herokuapp.com")
-os.environ.setdefault("DATABASE_URL", "postgres://your_username:your_password@localhost:5432/your_database_name")
