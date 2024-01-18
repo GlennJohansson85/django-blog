2f55ac3d08d88f32f48a5716ea9c66cf89ffa6b7
GlennJohansson85 <139962883+GlennJohansson85@users.noreply.github.com>
2024-01-18 22:05:50 +0100

    dff

--- env.py
+++ env.py
@@ -1,5 +1,14 @@
 import os
 
-os.environ.setdefault(
-    "DATABASE_URL", "postgres://ygkwkcqh:***@snuffleupagus.db.elephantsql.com/ygkwkcqh"
-)
+os.environ.setdefault("DATABASE_URL", "postgres://ygkwkcqh:***@snuffleupagus.db.elephantsql.com/ygkwkcqh")
+
+
+# Set the DEBUG mode (True for development, False for production)
+DEBUG = True
+
+# Add any other Django settings or custom environment variables you need
+# For example:
+# DATABASE_URL = 'your-database-url'
+
+# Make sure to add the following lines to the end of your env.py
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codestar.settings')
