c516fc468402c226ac2d447f13e1625435b2fda4
GlennJohansson85 <139962883+GlennJohansson85@users.noreply.github.com>
2024-01-19 14:20:48 +0100

    Update settings.py for Heroku

---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgres://ygkwkcqh:K5iV46-vAXnb-70h49jdixQQmUiqBfs3@snuffleupagus.db.elephantsql.com/ygkwkcqh")
+os.environ.setdefault(
+    "SECRET_KEY", "(j(j_+@%s$a955-5gw=m@b-%#$slmv$0aixrck&odnq*h+ig@0")
