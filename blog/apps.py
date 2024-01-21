6e2fbb11690bba44a6d86ac504d290558ace5e78
GlennJohansson85 <139962883+GlennJohansson85@users.noreply.github.com>
2024-01-21 01:29:42 +0100

    dynos works not. fixing it

--- blog/apps.py
+++ blog/apps.py
@@ -1,6 +1,14 @@
+# blog/apps.py
 from django.apps import AppConfig
-
 
 class BlogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'blog'
+
+    def ready(self):
+        # Import SummernoteModelAdmin here to avoid "Models aren't loaded yet" issue
+        from django_summernote.admin import SummernoteModelAdmin
+        # Register your models with SummernoteModelAdmin here
+        from .models import Post, Comment
+        admin.site.register(Post, SummernoteModelAdmin)
+        admin.site.register(Comment)
