c305ec3795ec49a34e628210860b8f22c1a4262c
GlennJohansson85 <139962883+GlennJohansson85@users.noreply.github.com>
2024-01-21 21:45:07 +0100

    admin.py
    Dyno fail. Unsure why. Move django_summernote to the top.

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,8 @@
+#admin py
+from django_summernote.admin import SummernoteModelAdmin
 from django.contrib import admin
 from .models import Post, Comment
-from django_summernote.admin import SummernoteModelAdmin
+
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
