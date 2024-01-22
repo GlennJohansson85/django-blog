b10af5a701efa3de4845c349f0c9c1db4070df8c
GlennJohansson85 <139962883+GlennJohansson85@users.noreply.github.com>
2024-01-22 21:22:05 +0100

    Update env.py
    remove "

--- env.py
+++ env.py
@@ -1,4 +1,4 @@
 import os
 
 os.environ["DATABASE_URL"] = "postgres://ygkwkcqh:K5iV46-vAXnb-70h49jdixQQmUiqBfs3@snuffleupagus.db.elephantsql.com/ygkwkcqh"
-os.environ.setdefault("SECRET_KEY", """)
+os.environ.setdefault("SECRET_KEY", "")
